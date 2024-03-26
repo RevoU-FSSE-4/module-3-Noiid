e8f59d3eb1ef72b976d1f4a8067b09d7416817ed
Dion <mwilliawarma13@gmail.com>
2024-03-26 22:06:34 +0700

    feat: add handle submit task

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // Place your code here
 // Add any additional code necessary to fulfill the requirements of the assignment
+
+function addTask() {
+  //checking input new task is empty
+  const newTask = document.getElementById("newTask").value;
+  const listTask = document.getElementById("listTask");
+
+  const childTr = document.createElement("tr");
+  const childTd = document.createElement("td");
+  const textNode = document.createTextNode(newTask);
+  childTd.appendChild(textNode);
+  childTr.appendChild(childTd);
+
+  listTask.appendChild(childTr);
+  alert("successfully adding a task!");
+}
